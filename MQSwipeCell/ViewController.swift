98a5b500db44183dd9d24ef31f8766f4e1f4029a
AidyBao <aidybao555@sina.com>
2018-09-04 09:45:20 +0800

    Initial Commit

---
+++ MQSwipeCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MQSwipeCell
+//
+//  Created by 120v on 2018/9/4.
+//  Copyright © 2018年 MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
